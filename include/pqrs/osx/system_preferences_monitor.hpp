20a2dbea475405bd7e72de434dadd359465b8370
Takayama Fumihiko <tekezo@pqrs.org>
2019-03-14 02:15:27 +0900

    version 1.0.0

--- include/pqrs/osx/system_preferences_monitor.hpp
+++ include/pqrs/osx/system_preferences_monitor.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-// pqrs::osx::system_preferences_monitor v0.0
+// pqrs::osx::system_preferences_monitor v1.0
 
 // (C) Copyright Takayama Fumihiko 2019.
 // Distributed under the Boost Software License, Version 1.0.
