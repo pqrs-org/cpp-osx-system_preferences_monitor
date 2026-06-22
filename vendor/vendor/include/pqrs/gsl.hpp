4de24fdf55b109612149721c7f6cb8466881f2b6
Takayama Fumihiko <tekezo@pqrs.org>
2026-06-22 19:48:26 +0900

    Update vendor

---
+++ vendor/vendor/include/pqrs/gsl.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+// pqrs::gsl v1.2.0
+
+// (C) Copyright Takayama Fumihiko 2025.
+// Distributed under the Boost Software License, Version 1.0.
+// (See https://www.boost.org/LICENSE_1_0.txt)
+
+#include <gsl/gsl>
+
+#include <cstddef>
+#include <functional>
+#include <memory>
+
+namespace pqrs {
+
+template <typename T>
+using not_null_shared_ptr_t = gsl::not_null<std::shared_ptr<T>>;
+
+template <typename T>
+[[nodiscard]] inline std::shared_ptr<T> unwrap_not_null(not_null_shared_ptr_t<T> p) noexcept {
+  return p.get();
+}
+
+template <typename T>
+[[nodiscard]] inline std::weak_ptr<T> make_weak(not_null_shared_ptr_t<T> p) noexcept {
+  return std::weak_ptr<T>(p.get());
+}
+
+} // namespace pqrs
+
+namespace std {
+// This definition is required to prevent the following error:
+//
+// ```
+// error: call to deleted constructor of 'gsl::not_null_hash<gsl::not_null<shared_ptr<T>>>'
+// ```
+template <typename T>
+struct hash<pqrs::not_null_shared_ptr_t<T>> final {
+  [[nodiscard]] std::size_t operator()(const pqrs::not_null_shared_ptr_t<T>& value) const noexcept(noexcept(std::hash<T>{}(*value))) {
+    return std::hash<T>{}(*value);
+  }
+};
+} // namespace std
